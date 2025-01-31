e32f08bb895bdea39572a65bf5fc34ac65ddc5d5
Syed Humza <105381562+syedhumzasalman@users.noreply.github.com>
2025-01-31 06:19:35 +0500

    starting Javascript

---
+++ app.js
@@ -0,0 +1,18 @@
+
+
+
+function displayTime() {
+    let hours = 0;
+    let minutes = 0;
+    let seconds = 1;
+    let watch;
+    
+    document.getElementById("watch").value = watch;
+}
+let clearset = setInterval(displayTime, 1000);
+
+function cleardisplayTime(){
+    clearInterval(clearset);
+}
+
+
