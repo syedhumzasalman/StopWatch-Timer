c5b83bd7b7a37b8af413ecf6f6c37299842e404a
Syed Humza <105381562+syedhumzasalman@users.noreply.github.com>
2025-02-02 05:05:08 +0500

    added stop watch function

--- app.js
+++ app.js
@@ -1,18 +1,68 @@
 
+// function stopwatch(){
+//     document.getElementById("stopWatch-section").innerHTML 
+// }
 
 
-function displayTime() {
-    let hours = 0;
-    let minutes = 0;
-    let seconds = 1;
-    let watch;
-    
-    document.getElementById("watch").value = watch;
+let secs = 0;
+let mins = 0;
+let hours = 0;
+let interval = 0;
+let display;
+let isPlaying = false;
+
+
+function displayTime(){
+    if(!isPlaying){
+    interval = setInterval(() => {
+        
+        secs++
+        if(secs == 60){
+            secs = 0;
+            mins++
+        }
+        if(mins == 60){
+            mins = 0;
+            hours++
+        }
+
+        if (mins < 60) {
+            display = `${String(mins).padStart(2, "0")}:${String(secs).padStart(2, "0")}`;
+        } 
+        
+        else {
+            display = `${String(hours).padStart(2, "0")}:${String(mins).padStart(2, "0")}:${String(secs).padStart(2, "0")}`;
+        }
+
+
+        document.getElementById("display").value = display;
+    }, 1000);
+
+    document.getElementById("play").innerHTML = '<i class="fa-solid fa-pause"></i>';
+    isPlaying = true;
+    document.getElementById("end").disabled = false;
+}else {
+        clearInterval(interval);
+        document.getElementById("play").innerHTML = '<i class="fa-solid fa-play"></i>';
+        isPlaying = false;
+    }
 }
-let clearset = setInterval(displayTime, 1000);
 
-function cleardisplayTime(){
-    clearInterval(clearset);
-}
 
+// function stopwatch(){
+//     clearInterval(interval)
+//     play.disabled = false
+// }
+
+
+
+function cleardisplayTime() {
+    clearInterval(interval);
+    secs = 0;
+    mins = 0;
+    hours = 0;
+    document.getElementById("display").value = "00:00"
+    document.getElementById("play").innerHTML = '<i class="fa-solid fa-play"></i>';
+    end.disabled = true
+  }
 
